9b6e382730a772964490a5b0f2cda57c2323722a
SteveAsterAfovo <steveasterafovo@gmail.com>
2025-09-19 15:02:11 +0100

    seo

--- app.vue
+++ app.vue
@@ -8,7 +8,42 @@
 </template>
 
 <script setup lang="ts">
+const runtimeConfig = useRuntimeConfig();
+const sharedFilesUrl = runtimeConfig.public.pgsSharedFiles;
+
+const heroImagePath = `${sharedFilesUrl}/PGS_Landing/Hero/pgs-banner.png`;
+const baseUrl = "https://progestionsoft.netlify.app";
+
 useHead({
   titleTemplate: '%s | PRO GESTION SOFT',
-})
+  meta: [
+    { charset: 'utf-8' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    {
+      key: 'description',
+      name: 'description',
+      content: 'Pro Gestion Soft développe des solutions numériques innovantes pour accompagner les PME africaines dans leur transformation digitale.'
+    },
+    { name: 'author', content: 'Pro Gestion Soft' },
+    { name: 'keywords', content: 'Pro Gestion Soft, numérique, PME, Afrique, Bénin, SaaS, digitalisation, technologie, PGS, SuitOps' },
+    
+    // Open Graph (Facebook, LinkedIn)
+    { property: 'og:title', content: 'Pro Gestion Soft - Solutions numériques pour les PME africaines' },
+    { property: 'og:description', content: 'Découvrez l’écosystème PGS : des outils innovants pour digitaliser votre entreprise.' },
+    { property: 'og:image', content: heroImagePath },
+    { property: 'og:url', content: baseUrl },
+    { property: 'og:type', content: 'website' },
+    { property: 'og:locale', content: 'fr_FR' },
+
+    // Twitter Card
+    { name: 'twitter:card', content: 'summary_large_image' },
+    { name: 'twitter:title', content: 'Pro Gestion Soft - Solutions numériques pour les PME africaines' },
+    { name: 'twitter:description', content: 'Découvrez l’écosystème PGS : des outils innovants pour digitaliser votre entreprise.' },
+    { name: 'twitter:image', content: heroImagePath },
+  ],
+  link: [
+    { rel: 'icon', type: 'image/png', href: '/favicon.ico' },
+    { rel: 'canonical', href: baseUrl }
+  ]
+});
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,6 +14,13 @@ export default defineNuxtConfig({
     '@nuxtjs/sitemap',
   ],
 
+  site: {
+    url: 'https://progestionsoft.netlify.app',
+    name: 'PRO GESTION SOFT',
+    gzip: true,
+    xsl: false,
+  },
+
   runtimeConfig: {
     public: {
       pgsBaseAPI: process.env.PGS_API_URL,
@@ -37,31 +44,16 @@ export default defineNuxtConfig({
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        {
-          hid: 'description',
-          name: 'description',
-          content: 'Pro Gestion Soft développe des solutions numériques innovantes pour accompagner les PME africaines dans leur transformation digitale.'
-        },
         { name: 'author', content: 'Pro Gestion Soft' },
         { name: 'keywords', content: 'Pro Gestion Soft, numérique, PME, Afrique, Bénin, SaaS, digitalisation, technologie, PGS, SuitOps' },
-
-        // Open Graph (Facebook, LinkedIn)
-        { property: 'og:title', content: 'Pro Gestion Soft - Solutions numériques pour les PME africaines' },
-        { property: 'og:description', content: 'Découvrez l’écosystème PGS : des outils innovants pour digitaliser votre entreprise.' },
-        { property: 'og:image', content: 'https://raw.githubusercontent.com/ProGestionSoft/Files/main/PGS_Landing/Hero/pgs-banner.png' },
-        { property: 'og:url', content: 'https://progestionsoft.com' },
-        { property: 'og:type', content: 'website' },
-        { property: 'og:locale', content: 'fr_FR' },
-
-        // Twitter Card
-        { name: 'twitter:card', content: 'summary_large_image' },
-        { name: 'twitter:title', content: 'Pro Gestion Soft - Solutions numériques pour les PME africaines' },
-        { name: 'twitter:description', content: 'Découvrez l’écosystème PGS : des outils innovants pour digitaliser votre entreprise.' },
-        { name: 'twitter:image', content: 'https://raw.githubusercontent.com/ProGestionSoft/Files/main/PGS_Landing/Hero/pgs-banner.png' },
+        {
+          name: 'google-site-verification',
+          content: 'OdKxHpVkBSxk0mj4vD4OTmZPdVi5pWzyCu4QPIMHy9A'
+        },
       ],
       link: [
         { rel: 'icon', type: 'image/png', href: '/favicon.ico' },
-        { rel: 'canonical', href: 'https://progestionsoft.com' }
+        { rel: 'canonical', href: 'https://progestionsoft.netlify.app' }
       ]
     }
   }
